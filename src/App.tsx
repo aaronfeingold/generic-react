83be043d2a30beb3b506adca4cf20d656237b638
aaronfeingold <ajfeingold88@gmail.com>
2024-08-20 18:03:46 -0400

    add an interface for the box props...shouldn't be required tho, plus a callback fn for the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./App.css";
 
-const Heading = ({ title }: { title: string }) => {
-  <h2>{title}</h2>;
-};
+const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
 
-const Box: React.FunctionComponent = ({ children }) => {
+interface BoxProps {
+  children: React.ReactNode;
+}
+
+const Box: React.FunctionComponent<BoxProps> = ({ children }) => (
   <div
     style={{
       padding: "1rem",
@@ -13,14 +15,31 @@ const Box: React.FunctionComponent = ({ children }) => {
     }}
   >
     {children}
-  </div>;
-};
+  </div>
+);
+
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item: string) => void;
+}> = ({ items, onClick }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello There</Box>
+      <List items={["Item 1", "Item 2", "Item 3"]} onClick={onListClick} />
     </div>
   );
 }
