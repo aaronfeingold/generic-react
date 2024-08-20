1cf7bc2134269036af93a06f7126a99b96dbce4e
aaronfeingold <ajfeingold88@gmail.com>
2024-08-20 17:44:09 -0400

    pass down props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 
-const Heading = () => {
-  <h2>Heading</h2>;
+const Heading = (props: { title: string }) => {
+  <h2>{props.title}</h2>;
 };
 
 function App() {
   return (
     <div>
-      <Heading />
+      <Heading title="Introduction" />
     </div>
   );
 }
