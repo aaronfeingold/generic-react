74572032324c7ac05cd77773446099c9b43d61cd
aaronfeingold <ajfeingold88@gmail.com>
2024-08-20 18:11:26 -0400

    begin todo list by using useReducer, plus some data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import "./App.css";
 
 const Heading = ({ title }: { title: string }) => <h2>{title}</h2>;
@@ -31,15 +31,35 @@ const List: React.FunctionComponent<{
   </ul>
 );
 
+interface Payload {
+  text: string;
+}
+
+interface Todo {
+    id: number;
+    done: boolean;
+    text: string;
+}
+
 function App() {
   const onListClick = useCallback((item: string) => {
     alert(item);
   }, []);
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((json) => setPayload(json));
+  }, []);
+
+  const [todos, dispatch] = useReducer(todos: Todo[])
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Hello There</Box>
       <List items={["Item 1", "Item 2", "Item 3"]} onClick={onListClick} />
+      <Box>{JSON.stringify(payload)}</Box>
     </div>
   );
 }
